158aac1ef117591d0167869cc2a52b1e62a83938
evargas6156 <evargas39@ucmerced.edu>
2021-02-23 18:11:29 -0800

    .

--- pingTest.py
+++ pingTest.py
@@ -26,11 +26,11 @@ def main():
 
 
     # After sending a ping, simulate a little to prevent collision.
-    #s.runTime(1);
-    #s.ping(2, 3, "Hello, World");
+    s.runTime(1);
+    s.ping(2, 5, "Hello, World");
     s.runTime(10);
-    s.neighborDMP(2);
-    s.runTime(10);
+    #s.neighborDMP(2);
+    #s.runTime(10);
     #s.ping(1, 10, "Hi!");
     #s.runTime(1);
 
